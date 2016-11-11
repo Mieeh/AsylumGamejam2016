3d57ab6af82260b98f7694e60ab3458cedcd1c33
Mieeh <davidstyrbjorn@gmail.com>
2016-11-11 15:33:29 +0100

    Created Project

---
+++ Assets/Scripts/TerminalInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/*
+ * Will handle writing to the main input line
+ * And when entered is press it will send the input text string 
+ * To terminal handler where the command entered will be processed
+ *
+ *
+ * 
+ */
+
+public class TerminalInput : MonoBehaviour {
+
+    public Text inputText;
+
+
+
+}
